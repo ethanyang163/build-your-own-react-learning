799d18495bf41df27a86c81dd5374e1e1102ea37
Yuxiang Yang <yuxiang.yang@shanbay.com>
2021-03-01 22:43:31 +0800

    app without React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello',
+  },
+};
 
-const element = React.createElement('h1', { title: 'foo' }, 'Hello');
 const container = document.getElementById('root');
-ReactDOM.render(element, container);
+
+const node = document.createElement(element.type);
+node['title'] = element.props.title;
+
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
